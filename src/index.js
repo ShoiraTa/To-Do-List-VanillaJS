5f0526c91379a25e753890843239b1d7e8da6a8f
shoiraTa <shoira.shakirovna@gmail.com>
2021-10-28 10:11:15 +0500

    add true false validation on checkbox

--- src/index.js
+++ src/index.js
@@ -1,29 +1,94 @@
 import './style.css';
+import taskStatus from './taskStatus';
 
 const tasksUl = document.getElementById('tasks');
-const task = [{
-  description: 'description one',
-  completed: false,
-  index: 0,
-},
-{
-  description: 'description two',
-  completed: false,
-  index: 1,
-}];
+const inputField = document.getElementById('addTask');
+const clear = document.getElementById('clear');
+const checkbox = document.getElementsByClassName('checkbox');
 
-const display = (tasks) => {
-  let oneTask;
-  let element;
-  for (let i = 0; i < tasks.length; i += 1) {
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    element = document.createElement('li');
-    oneTask = tasks[i].description;
-    element.innerHTML = `<input type="checkbox" name="" id="checkbox"> <p> ${oneTask} </p> </input>`;
-    tasksUl.appendChild(element);
-  }
-  return element;
+let tasks = [];
+taskStatus();
+
+const outputTasks = () => {
+  const taskList = JSON.parse(localStorage.getItem('tasks'));
+  tasksUl.innerHTML = '';
+  const checkBox = document.createElement('input');
+  checkBox.type = 'checkbox';
+  checkBox.className = 'checkbox';
+
+  taskList.forEach((task, index) => {
+    tasksUl.appendChild(checkBox);
+    tasksUl.innerHTML += `<li> ${task.description}</li>`;
+  });
 };
 
-display(task);
+const displayOnLoad = () => {
+  if (localStorage.getItem('tasks') != null) {
+    outputTasks();
+  }
+};
+displayOnLoad();
+
+function updateCheckboxStatus(status, index) {
+  const taskList = JSON.parse(localStorage.getItem('tasks'));
+  if (status) {
+    taskList.forEach((item, i) => {
+      if (i === index) {
+        item.completed = true;
+        localStorage.setItem('tasks', JSON.stringify(taskList));
+        console.log('correct', localStorage);
+      }
+    });
+  } else {
+    taskList.forEach((item, i) => {
+      if (i === index) {
+        item.completed = false;
+        localStorage.setItem('tasks', JSON.stringify(taskList));
+        console.log('false', localStorage);
+      }
+    });
+  }
+}
+
+const addToList = (newTask) => {
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+  tasks.push(newTask);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+const tasksArr = () => {
+  const inputFieldValue = inputField.value;
+  const newTask = {
+    description: inputFieldValue,
+    completed: false,
+  };
+  addToList(newTask);
+};
+
+for (let i = 0; i < checkbox.length; i += 1) {
+  checkbox[i].addEventListener('click', () => {
+    const { checked } = checkbox[i];
+    if (checked) {
+      updateCheckboxStatus(true, i);
+    } else {
+      updateCheckboxStatus(false, i);
+    }
+  });
+}
+
+inputField.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    tasksArr();
+    outputTasks();
+  }
+});
+
+clear.addEventListener('click', () => {
+  localStorage.clear();
+  outputTasks();
+});
+updateCheckboxStatus(0);
