38b46da7212c8d3c01b08012e6d25ce87618c089
shoiraTa <shoira.shakirovna@gmail.com>
2021-10-28 19:51:21 +0500

    add location.reload on function

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import './style.css';
 import domObjects from './domObjects.js';
 import taskStatus from './taskStatus.js';
 import addNewTask from './addNewTask.js';
-import updateContent from './updateContent';
-import removeTask from './removeTask';
+import updateContent from './updateContent.js';
+import removeTask from './removeTask.js';
 
 const clear = document.getElementById('clear');
 const inputField = document.getElementById('addTask');
 const addTaskbtn = document.getElementById('addTaskbtn');
-
+removeTask();
 const displayOnLoad = () => {
   if (localStorage.getItem('tasks') != null) {
     domObjects();
@@ -16,22 +16,21 @@ const displayOnLoad = () => {
 };
 displayOnLoad();
 
-addTaskbtn.addEventListener('click', () => {
+const execute = () => {
   addNewTask();
   domObjects();
   taskStatus();
   updateContent();
   removeTask();
   inputField.value = '';
+};
+
+addTaskbtn.addEventListener('click', () => {
+  execute();
 });
 inputField.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
-    addNewTask();
-    domObjects();
-    taskStatus();
-    updateContent();
-    removeTask();
-    inputField.value = '';
+    execute();
   }
 });
 
@@ -41,6 +40,5 @@ clear.addEventListener('click', () => {
 });
 
 removeTask();
-
 taskStatus(0);
 updateContent();
--- src/removeTask.js
+++ src/removeTask.js
@@ -1,6 +1,7 @@
 import taskStatus from './taskStatus.js';
 import domObjects from './domObjects.js';
 
+const deletebtn = document.getElementsByClassName('fa-trash-alt');
 function removefromlist(index) {
   const taskList = JSON.parse(localStorage.getItem('tasks'));
   taskList.splice(index, 1);
@@ -10,10 +11,10 @@ function removefromlist(index) {
 }
 
 const deleteItem = () => {
-  const deletebtn = document.getElementsByClassName('fa-trash-alt');
   for (let i = 0; i < deletebtn.length; i += 1) {
     deletebtn[i].addEventListener('click', () => {
       removefromlist(i);
+      window.location.reload();
     });
   }
 };
