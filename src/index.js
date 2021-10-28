a918bc37c2b86559932a774cc747a305dcbc0ba7
shoiraTa <shoira.shakirovna@gmail.com>
2021-10-28 13:01:22 +0500

    update task status on local storage

--- src/index.js
+++ src/index.js
@@ -2,23 +2,31 @@ import './style.css';
 import taskStatus from './taskStatus';
 
 const tasksUl = document.getElementById('tasks');
+
 const inputField = document.getElementById('addTask');
 const clear = document.getElementById('clear');
 const checkbox = document.getElementsByClassName('checkbox');
-
 let tasks = [];
 taskStatus();
 
 const outputTasks = () => {
   const taskList = JSON.parse(localStorage.getItem('tasks'));
-  tasksUl.innerHTML = '';
+  const taskDiv = document.createElement('div');
   const checkBox = document.createElement('input');
+
+  taskDiv.className = 'd-flex';
+  tasksUl.innerHTML = '';
   checkBox.type = 'checkbox';
   checkBox.className = 'checkbox';
-
   taskList.forEach((task, index) => {
+    if (task.completed) {
+      checkBox.setAttribute('checked', 'checked');
+    } else {
+      checkBox.removeAttribute('checked', 'checked');
+    }
+    tasksUl.appendChild(taskDiv);
     tasksUl.appendChild(checkBox);
-    tasksUl.innerHTML += `<li> ${task.description}</li>`;
+    tasksUl.innerHTML += task.completed ? `<li> ${task.description} </li>` : `<li>${task.description}</li>`;
   });
 };
 
@@ -29,17 +37,9 @@ const displayOnLoad = () => {
 };
 displayOnLoad();
 
-function updateCheckboxStatus(status, index) {
+function updateCompletedInStorage(status, index) {
   const taskList = JSON.parse(localStorage.getItem('tasks'));
-  if (status) {
-    taskList.forEach((item, i) => {
-      if (i === index) {
-        item.completed = true;
-        localStorage.setItem('tasks', JSON.stringify(taskList));
-        console.log('correct', localStorage);
-      }
-    });
-  } else {
+  if (!status) {
     taskList.forEach((item, i) => {
       if (i === index) {
         item.completed = false;
@@ -47,7 +47,26 @@ function updateCheckboxStatus(status, index) {
         console.log('false', localStorage);
       }
     });
+  } else {
+    taskList.forEach((item, i) => {
+      if (i === index) {
+        item.completed = true;
+        console.log('correct', localStorage, index);
+        localStorage.setItem('tasks', JSON.stringify(taskList));
+      }
+    });
   }
+}
+
+for (let i = 0; i < checkbox.length; i += 1) {
+  checkbox[i].addEventListener('click', () => {
+    const { checked } = checkbox[i];
+    if (checked) {
+      updateCompletedInStorage(true, i);
+    } else {
+      updateCompletedInStorage(false, i);
+    }
+  });
 }
 
 const addToList = (newTask) => {
@@ -69,17 +88,6 @@ const tasksArr = () => {
   addToList(newTask);
 };
 
-for (let i = 0; i < checkbox.length; i += 1) {
-  checkbox[i].addEventListener('click', () => {
-    const { checked } = checkbox[i];
-    if (checked) {
-      updateCheckboxStatus(true, i);
-    } else {
-      updateCheckboxStatus(false, i);
-    }
-  });
-}
-
 inputField.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
     tasksArr();
@@ -91,4 +99,4 @@ clear.addEventListener('click', () => {
   localStorage.clear();
   outputTasks();
 });
-updateCheckboxStatus(0);
+updateCompletedInStorage(0);
